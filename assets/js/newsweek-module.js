7075e441abe7fbe3442accaa20c6eab52e2ffeea
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-15 23:47:57 +0200

    Get heights of divs in first row

--- assets/js/newsweek-module.js
+++ assets/js/newsweek-module.js
@@ -2,16 +2,37 @@
 
 var myModule = (function($){
 
+  let model = {};
+
   // cache dom
   let $newsWeekModule = $('#newsWeekModule');
   let $linksnav = $newsWeekModule.find('#links-nav');
   let $search = $newsWeekModule.find('#search');
 
+  let $firstRow = $newsWeekModule.find('#first-row');
+  // let $firstCol = $firstRow.children()
+
+
   // bind events
+  // document.onscroll(evt)
   $newsWeekModule.delegate($search, 'focusin', _hideNavBar);
   $newsWeekModule.delegate($search, 'focusout', _showNavBar);
 
   // behavior - private funcs
+  const _getSlidingDivHeights = () =>  [...$firstRow.children()].reduce((a, c, i) => {
+      const height = [...$(c).children()].reduce((ca, cc) => ca + $(cc).height(), 0);
+      a[i] = height;
+      return a;
+    }, [0, 0, 0]);
+
+  function _handleSliders() {
+    console.log([...$firstRow.children()].reduce((a, c, i) => {
+      const height = [...$(c).children()].reduce((ca, cc) => ca + $(cc).height(), 0);
+      a[i] = height;
+      return a;
+    }, [0, 0, 0]));
+  }
+
   function _hideNavBar() {
     $search.addClass('w-100');
     $linksnav.hide(1000, 'swing');
@@ -25,6 +46,12 @@ var myModule = (function($){
   // can pass more stuff into the init - public func
   const init = () => {
     console.log('initializing newsweek module');
+    model.firstRowHeight = $firstRow.height();
+    model.slidingDivs = _getSlidingDivHeights();
+    console.log(model);
+    console.log();
+    _handleSliders();
+    
   }
 
   return {
